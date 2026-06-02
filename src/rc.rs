6ed5fa9f45d597632ed8bf7b46849cadcf538ef0
Yibo Cao <cyb@ieee.org>
2026-06-02 08:31:05 -0400

    tests & Fn blanket

--- src/rc.rs
+++ src/rc.rs
@@ -9,6 +9,10 @@ pub trait Listener<T> {
     fn accept(&self, event: &T);
 }
 
+impl<T, F: Fn(&T)> Listener<T> for F {
+    fn accept(&self, event: &T) { self(event) }
+}
+
 pub struct Registry<T> {
     head: Cell<*const ()>,
     _p: PhantomData<fn(T)>,
@@ -125,5 +129,149 @@ impl<T> Registry<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate std;
+
+    use super::{Listener, Registry};
+    use alloc::rc::Rc;
+    use core::array;
+    use core::cell::{Cell, OnceCell};
+    use core::pin::{Pin, pin};
+
+    #[derive(Default)]
+    struct State {
+        accept_count: Cell<u32>,
+        drop_count: Cell<u32>,
+        sum: Cell<u32>,
+    }
+
+    struct Capturer(Rc<State>);
+
+    impl Listener<u32> for Capturer {
+        fn accept(&self, event: &u32) {
+            self.0.accept_count.update(|x| x + 1);
+            self.0.sum.update(|x| x + event);
+        }
+    }
+
+    impl Drop for Capturer {
+        fn drop(&mut self) { self.0.drop_count.update(|x| x + 1); }
+    }
+
+    #[test]
+    fn normal_path() {
+        let reg = pin!(Registry::new());
+        let reg = reg.as_ref();
+        let states = <[Rc<State>; 3]>::default();
+        let _guards = states.each_ref().map(|x| reg.register(Capturer(x.clone())));
+        reg.broadcast(&3);
+        reg.broadcast(&4);
+        assert_eq!(states.each_ref().map(|x| x.accept_count.get()), [2, 2, 2]);
+        assert_eq!(states.each_ref().map(|x| x.sum.get()), [7, 7, 7]);
+    }
+
+    #[test]
+    fn registry_dropped_early() {
+        let state = Rc::<State>::default();
+        let _guards: [_; 2];
+        {
+            let reg = pin!(Registry::new());
+            let reg = reg.as_ref();
+            _guards = array::from_fn(|_| reg.register(Capturer(state.clone())));
+        }
+        assert_eq!(state.drop_count.get(), 2);
+    }
+
+    #[test]
+    fn registry_dropped_late() {
+        let state = Rc::<State>::default();
+        let reg = pin!(Registry::new());
+        let reg = reg.as_ref();
+        let guards: [_; 3] = array::from_fn(|_| reg.register(Capturer(state.clone())));
+        drop(guards);
+        assert_eq!(state.drop_count.get(), 3);
+    }
+
+    #[test]
+    fn cancel_internal() {
+        let state = Rc::<State>::default();
+        let reg = pin!(Registry::new());
+        let reg = reg.as_ref();
+        let [_a, b, _c] = array::from_fn(|_| reg.register(Capturer(state.clone())));
+        drop(b);
+        assert_eq!(state.drop_count.get(), 1);
+        reg.broadcast(&5);
+        assert_eq!(state.sum.get(), 10);
+    }
+
+    #[test]
+    fn register_inside_callback() {
+        let reg = Rc::<Registry<u32>>::default();
+        let state = Rc::<State>::default();
+        let _g = unsafe { Pin::new_unchecked(&*reg) }.register({
+            let (reg, state, capturer) = (Rc::downgrade(&reg), state.clone(), OnceCell::new());
+            move |_: &u32| {
+                capturer.get_or_init(|| {
+                    let reg = reg.upgrade().unwrap();
+                    unsafe { Pin::new_unchecked(&*reg) }.register(Capturer(state.clone()))
+                });
+            }
+        });
+        reg.broadcast(&0);
+        assert_eq!(state.accept_count.get(), 0);
+        reg.broadcast(&0);
+        assert_eq!(state.accept_count.get(), 1);
+    }
+
+    #[test]
+    fn self_cancel() {
+        let reg = pin!(Registry::new());
+        let reg = reg.as_ref();
+        let state = Rc::<State>::default();
+        let guard = Rc::new(Cell::new(None));
+        guard.set(Some(reg.register({
+            let (state, me) = (state.clone(), guard.clone());
+            move |_: &u32| {
+                state.accept_count.update(|x| x + 1);
+                me.set(None);
+            }
+        })));
+        reg.broadcast(&0);
+        assert_eq!(state.accept_count.get(), 1);
+        reg.broadcast(&0);
+        assert_eq!(state.accept_count.get(), 1);
+    }
+
+    #[test]
+    fn cancel_in_flight() {
+        let reg = pin!(Registry::new());
+        let reg = reg.as_ref();
+        let state = Rc::<State>::default();
+        let victim = Cell::new(Some(reg.register(Capturer(state.clone()))));
+        let _g = reg.register(move |_: &u32| victim.set(None));
+        reg.broadcast(&0);
+        assert_eq!(state.accept_count.get(), 0);
+        assert_eq!(state.drop_count.get(), 1);
+        reg.broadcast(&0);
+        assert_eq!(state.accept_count.get(), 0);
+    }
+
+    #[test]
+    fn nested_broadcast_is_safe() {
+        let reg = Rc::<Registry<u32>>::default();
+        let state = Rc::<State>::default();
+        let _g = unsafe { Pin::new_unchecked(&*reg) }.register({
+            let (reg, state, depth) = (Rc::downgrade(&reg), state.clone(), Cell::new(0u32));
+            move |event: &u32| {
+                state.accept_count.update(|x| x + 1);
+                if depth.replace(depth.get() + 1) < 2 {
+                    reg.upgrade().unwrap().broadcast(event);
+                }
+            }
+        });
+        reg.broadcast(&0);
+        assert_eq!(state.accept_count.get(), 3);
     }
 }
